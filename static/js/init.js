f971a3fd94fe31682d9404f27f1614f99aef9e17
陈一平 <403791515@qq.com>
2018-03-20 14:12:55 +0800

    Adds mobile-friendly navigation menu and various tweaks
    1. Clicking on items on the navbar no longer refreshes the webpage itself.
    
    2. Link in the Events section has been fixed.
    
    3. Code cleanup. Code that should be put into individual webpages instead of a global js file have been separated.

--- static/js/init.js
+++ static/js/init.js
@@ -1,23 +1,4 @@
-var swiper = new Swiper('.swiper-container', {
-  on: {
-    slideChangeTransitionEnd: function(){
-      // console.log(this.activeIndex)
-      document.querySelector('.mynav').style.opacity = this.activeIndex ? '1' : '0'
-    },
-  },
-  direction: 'vertical',
-  keyboard: {
-    enabled: true,
-  },
-  mousewheel: true,
-});
-
-$(document).keydown(function (event) {
-  if (event.target.nodeName == 'TEXTAREA' || event.target.nodeName == 'INPUT') {
-    return;
-  };
-  /* Act on the event */
-  if (event.keyCode == 32) {
-    event.preventDefault();
-  };
+$(document).ready(function () {
+  $('.sidenav').sidenav();
+  $('.sidenav-overlay').css({ 'z-index': 2 })
 });
